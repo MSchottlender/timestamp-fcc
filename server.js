316691dd891d2b92aeb09c63c5211db19f31f664
MSchottlender <martin.schottlender@gmail.com>
2019-09-27 09:38:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,21 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.get('/api/timestamp/:date_string?',(req,res) => {
+  //req.date_string = new Date().toString();
+  var today = new Date(req.params.date_string);
+  //console.log(today)
+  /*f(new Date(date_string)){
+    console.log(4)
+  }*/
+  //console.log(5)
+  if(today.getTime()){
+    res.send({unix: today.getTime(),
+              utc: today.toUTCString()});
+} else {
+      res.send({error: "Invalid Date"});
+    }
+  
+})
